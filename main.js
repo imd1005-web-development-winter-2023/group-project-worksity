9a5e99501c81c786a06dfa934eec63dd37992680
rayliable <rayrmrose@cmail.carleton.ca>
2023-04-11 01:04:57 -0400

    :(

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+function onMenuClick() {
+    alert("You Clicked me");
+    var navbar = document.getElementById("header--banner");
+    var responsive_class_name = "responsive";
+
+    navbar.classList.toggle(responsive_class_name);
+}
